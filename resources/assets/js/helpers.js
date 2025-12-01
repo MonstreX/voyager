c6c1e25d360b736fe129fc8f7999c9f5f72ad72e
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-01 18:24:18 +0500

    refactor helpers bootstrap alerter

--- resources/assets/js/helpers.js
+++ resources/assets/js/helpers.js
@@ -4,7 +4,7 @@
 |
 --------------------*/
 
-var displayAlert = function(alert, alerter) {
+const displayAlert = function(alert, alerter) {
     let alertMethod = alerter[alert.type];
 
     if (alertMethod) {
@@ -14,7 +14,7 @@ var displayAlert = function(alert, alerter) {
     alerter.error("No alert method found for alert type: " + alert.type);
 }
 
-var displayAlerts = function(alerts, alerter, type) {
+const displayAlerts = function(alerts, alerter, type) {
     if (type) {
         // Only display alerts of this type...
         alerts = alerts.filter(function(alert) {
@@ -22,37 +22,45 @@ var displayAlerts = function(alerts, alerter, type) {
         });
     }
 
-    for (a in alerts) {
-        displayAlert(alerts[a], alerter);
-    }
+    alerts.forEach(function(alert) {
+        displayAlert(alert, alerter);
+    });
 }
 
-var bootstrapAlerter = function(customOptions) {
-    // Default options
-    let options = {
+const bootstrapAlerter = function(customOptions = {}) {
+    const options = Object.assign({
         alertsContainer: '#alertsContainer',
         dismissible: false,
         dismissButton: '<button class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
-    };
+    }, customOptions);
 
-    if (customOptions) {
-        options = $.extend({}, options, customOptions);
-    }
+    const dismissibleClass = options.dismissible ? ' alert-dismissible' : '';
+    const dismissButton = options.dismissible ? options.dismissButton : '';
 
-    let dismissibleClass = '';
-    let dismissButton = '';
+    function resolveAlertsContainer() {
+        if (typeof options.alertsContainer === 'string') {
+            return document.querySelector(options.alertsContainer);
+        }
 
-    if (options.dismissible) {
-        dismissButton = options.dismissButton;
-        dismissibleClass = ' alert-dismissible';
+        if (options.alertsContainer instanceof HTMLElement) {
+            return options.alertsContainer;
+        }
+
+        return null;
     }
 
     function notify(type, message) {
-        let alert = '<div class="alert alert-'  + type +  dismissibleClass + '" role="alert">'
+        const container = resolveAlertsContainer();
+        if (!container) {
+            console.warn('bootstrapAlerter: alerts container not found');
+            return;
+        }
+
+        const alert = '<div class="alert alert-'  + type +  dismissibleClass + '" role="alert">'
                         + dismissButton + message +
                     '</div>';
 
-        $(options.alertsContainer).append(alert);
+        container.insertAdjacentHTML('beforeend', alert);
     }
 
     return {
@@ -71,8 +79,17 @@ var bootstrapAlerter = function(customOptions) {
     };
 }
 
-var setImageValue = function(url){
-  $('.mce-btn.mce-open').parent().find('.mce-textbox').val(url);
+const setImageValue = function(url) {
+    const openButton = document.querySelector('.mce-btn.mce-open');
+    if (!openButton || !openButton.parentElement) {
+        return;
+    }
+
+    const textbox = openButton.parentElement.querySelector('.mce-textbox');
+
+    if (textbox) {
+        textbox.value = url;
+    }
 }
 
 export { setImageValue, displayAlert, displayAlerts, bootstrapAlerter };
