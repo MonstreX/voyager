298c0deccb82be2904626081796dd878939b0a9d
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-01 18:00:58 +0500

    fix: transliterate cyrillic in slugify

--- resources/assets/js/modules/slugify.js
+++ resources/assets/js/modules/slugify.js
@@ -4,13 +4,30 @@
     forceUpdate: false
 };
 
+const ADDITIONAL_CHAR_MAP = {
+    // Russian / Ukrainian / Belarusian
+    а: 'a', б: 'b', в: 'v', г: 'g', д: 'd', е: 'e', ё: 'yo', ж: 'zh', з: 'z',
+    и: 'i', й: 'j', к: 'k', л: 'l', м: 'm', н: 'n', о: 'o', п: 'p', р: 'r',
+    с: 's', т: 't', у: 'u', ф: 'f', х: 'h', ц: 'c', ч: 'ch', ш: 'sh',
+    щ: 'sh', ъ: '', ы: 'y', ь: '', э: 'e', ю: 'yu', я: 'ya',
+    ї: 'yi', і: 'i', є: 'ye', ґ: 'g',
+    А: 'A', Б: 'B', В: 'V', Г: 'G', Д: 'D', Е: 'E', Ё: 'Yo', Ж: 'Zh', З: 'Z',
+    И: 'I', Й: 'J', К: 'K', Л: 'L', М: 'M', Н: 'N', О: 'O', П: 'P', Р: 'R',
+    С: 'S', Т: 'T', У: 'U', Ф: 'F', Х: 'H', Ц: 'C', Ч: 'Ch', Ш: 'Sh',
+    Щ: 'Sh', Ъ: '', Ы: 'Y', Ь: '', Э: 'E', Ю: 'Yu', Я: 'Ya',
+    Ї: 'Yi', І: 'I', Є: 'Ye', Ґ: 'G'
+};
+
 const slugifyInstances = new WeakMap();
 
 const slugifyValue = (value, separator = '-') => {
     const sep = separator || '-';
     const escapedSep = sep.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    const normalized = (value || '')
+    const transliterated = (value || '')
         .toString()
+        .split('')
+        .map((char) => (ADDITIONAL_CHAR_MAP[char] !== undefined ? ADDITIONAL_CHAR_MAP[char] : char))
+        .join('')
         .normalize('NFD')
         .replace(/[\u0300-\u036f]/g, '')
         .toLowerCase();
@@ -18,7 +35,7 @@ const slugifyValue = (value, separator = '-') => {
     const pattern = new RegExp(`\\${escapedSep}+`, 'g');
     const boundary = new RegExp(`^\\${escapedSep}+|\\${escapedSep}+$`, 'g');
 
-    return normalized
+    return transliterated
         .replace(/[^a-z0-9]/g, sep)
         .replace(pattern, sep)
         .replace(boundary, '');
