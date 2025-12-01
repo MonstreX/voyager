302b54b3c5f326ac5b5e7057ba74e9ddf9410109
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-01 15:51:42 +0500

    chore: remove jquery datepickers and matchHeight

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -29,10 +29,82 @@ const loadLegacyPlugin = (source, sourceName) => {
 import jqueryUiSource from './vendor/jquery-ui.js?raw';
 loadLegacyPlugin(jqueryUiSource, 'jquery-ui.js');
 
-import matchHeightSource from './vendor/jquery.matchHeight.js?raw';
-loadLegacyPlugin(matchHeightSource, 'jquery.matchHeight.js');
 import Sortable from 'sortablejs';
 window.Sortable = Sortable;
+
+const resolveMatchHeightGroups = () => {
+    if (typeof document === 'undefined') {
+        return [];
+    }
+    const groups = [];
+    const classElements = Array.from(document.querySelectorAll('.match-height'));
+    if (classElements.length > 1) {
+        groups.push(classElements);
+    }
+    const grouped = {};
+    document.querySelectorAll('[data-match-height], [data-mh]').forEach((element) => {
+        const key = element.getAttribute('data-match-height') || element.getAttribute('data-mh');
+        if (!key) {
+            return;
+        }
+        if (!grouped[key]) {
+            grouped[key] = [];
+        }
+        grouped[key].push(element);
+    });
+    Object.keys(grouped).forEach((key) => {
+        if (grouped[key].length > 1) {
+            groups.push(grouped[key]);
+        }
+    });
+    return groups;
+};
+
+const applyMatchHeight = () => {
+    resolveMatchHeightGroups().forEach((elements) => {
+        let maxHeight = 0;
+        elements.forEach((element) => {
+            element.style.minHeight = '';
+            element.style.height = '';
+            const rect = element.getBoundingClientRect();
+            if (rect.height > maxHeight) {
+                maxHeight = rect.height;
+            }
+        });
+        if (maxHeight <= 0) {
+            return;
+        }
+        elements.forEach((element) => {
+            element.style.minHeight = `${maxHeight}px`;
+        });
+    });
+};
+
+let matchHeightScheduled = false;
+const initMatchHeight = () => {
+    if (typeof window === 'undefined' || typeof document === 'undefined') {
+        return;
+    }
+    const scheduleUpdate = () => {
+        if (matchHeightScheduled) {
+            return;
+        }
+        matchHeightScheduled = true;
+        const runner = () => {
+            matchHeightScheduled = false;
+            applyMatchHeight();
+        };
+        if (typeof window.requestAnimationFrame === 'function') {
+            window.requestAnimationFrame(runner);
+        } else {
+            setTimeout(runner, 60);
+        }
+    };
+    applyMatchHeight();
+    window.addEventListener('resize', scheduleUpdate);
+};
+
+window.VoyagerInitMatchHeight = initMatchHeight;
 
 const logNestable = (...args) => {
     if (typeof console === 'undefined' || typeof console.debug !== 'function') {
@@ -1933,7 +2005,9 @@ $(document).ready(function () {
         }
     });
 
-    $('.match-height').matchHeight();
+    if (window.VoyagerInitMatchHeight) {
+        window.VoyagerInitMatchHeight();
+    }
 
     const sideMenuNav = document.querySelector('.side-menu .nav');
     if (sideMenuNav) {
