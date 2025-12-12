a23ee6448de4c8fdfe93d87129c5c44eab749d56
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-12 12:36:15 +0500

    Tree mode style fixes

--- resources/views/menu/admin.blade.php
+++ resources/views/menu/admin.blade.php
@@ -3,32 +3,36 @@
 @foreach ($items as $item)
 
     <li class="dd-item" data-id="{{ $item->id }}">
-        <div class="pull-right item_actions">
-            <div class="btn btn-sm btn-danger pull-right delete" data-id="{{ $item->id }}">
-                <i class="voyager-trash"></i> {{ __('voyager::generic.delete') }}
+        <div class="dd-handle dd-menu-item">
+            <div class="dd-menu-left">
+                @if($options->isModelTranslatable)
+                    @include('voyager::multilingual.input-hidden', [
+                        'isModelTranslatable' => true,
+                        '_field_name'         => 'title'.$item->id,
+                        '_field_trans'        => json_encode($item->getTranslationsOf('title'))
+                    ])
+                @endif
+                <span>{{ $item->title }}</span> <small class="url">{{ $item->link() }}</small>
             </div>
-            <div class="btn btn-sm btn-primary pull-right edit"
-                data-id="{{ $item->id }}"
-                data-title="{{ $item->title }}"
-                data-url="{{ $item->url }}"
-                data-target="{{ $item->target }}"
-                data-icon_class="{{ $item->icon_class }}"
-                data-color="{{ $item->color }}"
-                data-route="{{ $item->route }}"
-                data-parameters="{{ json_encode($item->parameters) }}"
-            >
-                <i class="voyager-edit"></i> {{ __('voyager::generic.edit') }}
+
+            <div class="item_actions bread-actions">
+                <a href="javascript:;" class="btn btn-sm btn-primary edit"
+                     data-id="{{ $item->id }}"
+                     data-title="{{ $item->title }}"
+                     data-url="{{ $item->url }}"
+                     data-target="{{ $item->target }}"
+                     data-icon_class="{{ $item->icon_class }}"
+                     data-color="{{ $item->color }}"
+                     data-route="{{ $item->route }}"
+                     data-parameters="{{ json_encode($item->parameters) }}"
+                     title="{{ __('voyager::generic.edit') }}"
+                >
+                    <i class="voyager-edit"></i>
+                </a>
+                <a href="javascript:;" class="btn btn-sm btn-danger pull-right delete" data-id="{{ $item->id }}" title="{{ __('voyager::generic.delete') }}">
+                    <i class="voyager-trash"></i>
+                </a>
             </div>
-        </div>
-        <div class="dd-handle">
-            @if($options->isModelTranslatable)
-                @include('voyager::multilingual.input-hidden', [
-                    'isModelTranslatable' => true,
-                    '_field_name'         => 'title'.$item->id,
-                    '_field_trans'        => json_encode($item->getTranslationsOf('title'))
-                ])
-            @endif
-            <span>{{ $item->title }}</span> <small class="url">{{ $item->link() }}</small>
         </div>
         @if(!$item->children->isEmpty())
             @include('voyager::menu.admin', ['items' => $item->children])
