20df1a8d51439933666466711b4bc232d7edb063
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-14 18:25:49 +0500

    Fix media delete modal nesting for Vue manager

--- resources/views/media/manager.blade.php
+++ resources/views/media/manager.blade.php
@@ -268,11 +268,11 @@
     <!-- End New Folder Modal -->
 
     <!-- Delete File Modal -->
-    <div class="modal fade modal-danger" v-if="allowDelete">
+    <div v-if="allowDelete">
         @include('voyager::components.modal-confirm', [
             'bindId' => "'confirm_delete_modal_'+uid",
             'title' => __('voyager::generic.are_you_sure'),
-            'message' => '<h4>'.__('voyager::media.delete_question').'</h4><ul><li v-for="file in selected_files">@{{ file.name }}</li></ul><h5 class=\"folder_warning\"><i class=\"voyager-warning\"></i> '.__('voyager::media.delete_folder_question').'</h5>',
+            'message' => '<h4>'.__('voyager::media.delete_question').'</h4><ul><li v-for=\"file in selected_files\">@{{ file.name }}</li></ul><h5 class=\"folder_warning\"><i class=\"voyager-warning\"></i> '.__('voyager::media.delete_folder_question').'</h5>',
             'confirmText' => __('voyager::generic.delete_confirm'),
             'confirmClass' => 'btn-danger',
             'bindConfirmButtonId' => "'confirm_delete_files_'+uid",
