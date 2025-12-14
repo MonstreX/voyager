12ce1c14188ec2c1264bb6d8f9159bf932482f21
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-14 17:09:15 +0500

    Allow HTML in confirm modal titles for bulk messages

--- resources/views/components/modal-confirm.blade.php
+++ resources/views/components/modal-confirm.blade.php
@@ -15,11 +15,11 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="{{ $cancelText }}"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">
-                    <i class="{{ $icon }}"></i> {{ $title }}
+                    <i class="{{ $icon }}"></i> {!! $title !!}
                 </h4>
             </div>
             <div class="modal-body">
-                <p>{!! $message !!}</p>
+                <div class="voyager-confirm-message">{!! $message !!}</div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default pull-right" data-dismiss="modal">{{ $cancelText }}</button>
