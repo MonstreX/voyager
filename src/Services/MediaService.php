d85fbbe663b9f37e521174597dd4b18a179b978e
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-14 12:40:36 +0500

    refactor: Use Str::slug() instead of custom transliteration table in MediaService

--- src/Services/MediaService.php
+++ src/Services/MediaService.php
@@ -4,6 +4,7 @@ namespace TCG\Voyager\Services;
 
 use TCG\Voyager\Models\Media;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MediaService
 {
@@ -91,57 +92,19 @@ class MediaService
         $extension = $file->getClientOriginalExtension();
         $nameWithoutExt = pathinfo($originalName, PATHINFO_FILENAME);
 
-        $sanitizedName = $this->sanitizeFileName($nameWithoutExt);
-        $transliteratedName = $this->transliterate($sanitizedName);
+        $slugName = Str::slug($nameWithoutExt, '_') ?: 'file';
 
-        $fileName = $transliteratedName . '.' . $extension;
+        $fileName = $slugName . '.' . $extension;
 
         if (!Storage::disk($disk)->exists($dirPath . '/' . $fileName)) {
             return $fileName;
         }
 
         $counter = 1;
-        while (Storage::disk($disk)->exists($dirPath . '/' . $transliteratedName . '_' . $counter . '.' . $extension)) {
+        while (Storage::disk($disk)->exists($dirPath . '/' . $slugName . '_' . $counter . '.' . $extension)) {
             $counter++;
         }
 
-        return $transliteratedName . '_' . $counter . '.' . $extension;
-    }
-
-    protected function transliterate(string $str): string
-    {
-        $translitMap = [
-            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
-            'е' => 'e', 'ё' => 'yo', 'ж' => 'zh', 'з' => 'z', 'и' => 'i',
-            'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
-            'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
-            'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
-            'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '',
-            'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
-            'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D',
-            'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh', 'З' => 'Z', 'И' => 'I',
-            'Й' => 'Y', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N',
-            'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T',
-            'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C', 'Ч' => 'Ch',
-            'Ш' => 'Sh', 'Щ' => 'Sch', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '',
-            'Э' => 'E', 'Ю' => 'Yu', 'Я' => 'Ya',
-        ];
-
-        $result = '';
-        foreach (mb_str_split($str) as $char) {
-            $result .= $translitMap[$char] ?? $char;
-        }
-
-        return $result;
-    }
-
-    protected function sanitizeFileName(string $fileName): string
-    {
-        $fileName = preg_replace('/[^\p{L}\p{N}\s._-]/u', '', $fileName);
-        $fileName = preg_replace('/\s+/', '_', trim($fileName));
-        $fileName = preg_replace('/_+/', '_', $fileName);
-        $fileName = trim($fileName, '_');
-
-        return $fileName ?: 'file';
+        return $slugName . '_' . $counter . '.' . $extension;
     }
 }
