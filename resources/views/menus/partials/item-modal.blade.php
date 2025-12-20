97da48a46b7f4792c55fd7fc7bd743e35ec838c2
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-20 20:59:07 +0500

    Menu builder: fix item modal title markup

--- resources/views/menus/partials/item-modal.blade.php
+++ resources/views/menus/partials/item-modal.blade.php
@@ -68,11 +68,9 @@
                         <textarea rows="3" class="form-control" id="m_parameters" name="parameters" placeholder="{{ json_encode(['key' => 'value'], JSON_PRETTY_PRINT) }}"></textarea><br>
                     </div>
 
-                    <label for="icon_class">{{ __('voyager::menu_builder.icon_class') }}
-                        <a href="{{ route('voyager.compass.index') }}#fonts" target="_blank">
-                            {!! __('voyager::menu_builder.icon_class2') !!}
-                        </a>
-                    </label>
+                    <label for="icon_class">{{ __('voyager::menu_builder.icon_class') }} <a
+                            href="{{ route('voyager.compass.index') }}#fonts"
+                            target="_blank">{!! __('voyager::menu_builder.icon_class2') !!}</label>
                     <input type="text" class="form-control" id="m_icon_class" name="icon_class"
                            placeholder="{{ __('voyager::menu_builder.icon_class_ph') }}"><br>
 
@@ -98,4 +96,3 @@
         </div>
     </div>
 </div>
-
